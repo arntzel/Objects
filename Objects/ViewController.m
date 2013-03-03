f9d03ebb907097bce236a38270034a3a1c7833aa
Eliot <arntzel@gmail.com>
2013-03-02 19:17:52 -0500

    initial commit

--- Objects/ViewController.m
+++ Objects/ViewController.m
@@ -17,7 +17,31 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    NSString *myString = [[NSString alloc] init];
+    myString = @"hello world";
+    
+//    NSLog(@"myStringLength: %i", myString.length);
+    int x = 10;
+    NSNumber *xNumber = [[NSNumber alloc] initWithInt:x];
+    
+    NSArray *myArray = [[NSArray alloc] init];
+    myArray = @[xNumber, myString];
+    NSString *myName = @"Eliot";
+    
+    NSMutableArray *myMutableArray = [[NSMutableArray alloc] init];    
+    [myMutableArray addObject:myString];    
+    [myMutableArray addObject:xNumber];
+    [myMutableArray addObject:myName];
+//    NSLog(@"count: %i", myMutableArray.count);
+//    [myMutableArray removeLastObject];
+//    [myMutableArray removeAllObjects];
+    UILabel *myLabel = [[UILabel alloc] init];
+    myLabel.text = @"Hello World";
+    NSLog(@"%@", myLabel.text);
+    
+
+    
 }
 
 - (void)didReceiveMemoryWarning
